740e493e890f9228afde8a01023f2172da68d3d9
ZainaDali <dalizaina19092000@gmail.com>
2025-09-01 16:06:16 +0200

    partie 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-list-app' title`, () => {
+  it('should have the todo-list-app title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('todo-list-app');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { TodoListComponent } from './features/todos/components/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, TodoListComponent],
+  template: `
+    <app-header></app-header>
+    <main class="container mx-auto p-4">
+      <app-todo-list></app-todo-list>
+    </main>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'todo-list-app';
